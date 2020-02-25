b07dd12cc190b7438541945d1ad5c71e8185c3d7
martin <telemgico@gmail.com>
2020-02-25 15:14:16 -0300

    fix pgn search

--- src/components/Live.vue
+++ src/components/Live.vue
@@ -134,8 +134,8 @@
           this.pages = pages
           this.$root.loading = false
           axios.post(this.$root.endpoint + '/eco/search', {query: this.query}).then((res2) => {
-            if(res2.data.games[0]){
-              t.eco = res2.data.games[0]
+            if(res2.data){
+              t.eco = res2.data
             }
           })
         })       
