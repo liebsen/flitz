30f2fa150c3d9b4773765753f41b189ac6982f02
martin <telemgico@gmail.com>
2020-11-13 09:25:24 -0300

    build

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,30 +1,18 @@
 <template>
   <div class="container is-widescreen landing">
     <div class="area has-text-centered preservefilter">
-      <div class="columns is-vcentered fadeIn">
+      <div class="columns is-vcentered is-padded-bottom fadeIn">
         <div class="column is-hidden-mobile"></div>
         <div class="column is-8 has-text-centered">
           <div class="has-text-centered">
             <form id="search" class="has-text-centered preservefilter" @submit.prevent="submit">
-              <div class="field has-addons is-hidden-mobile is-flex-centered">
-                <div class="control">
-                  <input v-model="query" class="input is-large is-white is-rounded" name="query" type="text" :placeholder="'search_in_groups' | t" autofocus>
-                </div>
-                <div class="control">
-                  <button type="submit" id="searchbtn" class="button is-large is-rounded is-white">
-                    <span class="icon has-text-info">
-                      <span class="mdi mdi-layers-search"></span>
-                    </span>
-                  </button>
-                </div>
-              </div>
-              <div class="field has-addons is-hidden-tablet is-flex-centered">
+              <div class="field has-addons is-flex-centered">
                 <div class="control">
                   <input v-model="query" class="input is-rounded" name="query" type="text" :placeholder="'search_in_groups' | t" autofocus>
                 </div>
                 <div class="control">
-                  <button type="submit" id="searchbtn" class="button is-rounded is-white">
-                    <span class="icon has-text-info">
+                  <button type="submit" id="searchbtn" class="button is-rounded is-success">
+                    <span class="icon has-text-white">
                       <span class="mdi mdi-layers-search"></span>
                     </span>
                   </button>
@@ -33,8 +21,8 @@
             </form>
             <div class="content columns is-multiline has-text-centered">
               <div v-for="(item, index) in groups" :key="index" class="column is-6-tablet is-4-desktop">
-                <router-link :to="`/group/${item._id}`">
-                  <article class="box preservefilter">
+                <router-link :to="`/group/${item._id}`"  class="box is-marginless">
+                  <article>
                     <h2>
                       <span>{{ item.code }}</span>
                       <span class="has-text-grey">{{ item.users }}</span>
@@ -48,26 +36,6 @@
                       <span>{{ item.compensation }}</span>
                     </p>
                   </article>
-                </router-link>
-              </div>
-            </div>
-            <div class="columns is-vcentered has-text-centered is-hidden-mobile">
-              <div class="column has-text-right">
-                <a @click="$root.play" class="button is-rounded is-large is-success">
-                  <span class="icon">
-                    <span class="fa fa-handshake is-size-5"></span>
-                  </span>
-                  <!--span class="is-hidden-touch">{{ 'play_against' | t }}</span-->
-                  <span>{{ 'human' | t }}</span>
-                </a>
-              </div>
-              <div class="column has-text-left">
-                <router-link class="button is-rounded is-info is-large" to="/stockfish">
-                  <span class="icon">
-                    <span class="fa fa-server is-size-5"></span>
-                  </span>
-                  <!--span class="is-hidden-touch">{{ 'play_against' | t }}&nbsp;</span-->
-                  <span>Stockfish</span>
                 </router-link>
               </div>
             </div>
